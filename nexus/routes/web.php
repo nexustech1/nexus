366ff42a576572e0a76d3ddda516131f0365e228
davi <fdavi07@gmail.com>
2025-04-15 14:32:04 -0300

    agora vai

--- nexus/routes/web.php
+++ nexus/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('site');
 });
 
-Route::get('/test', [TestController::class, 'test'])->name('test');
+Route::get('/site', [TestController::class, 'test'])->name('test');
