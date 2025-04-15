d055571152bf3fc6a60f31fabe7ddf5a79941ea1
Felipe <felpztws2206@gmail.com>
2025-04-15 15:26:48 -0300

    Mensagem de my commit

--- nexus/routes/web.php
+++ nexus/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\loginController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::get('/site', [TestController::class, 'test'])->name('test');
+Route::get('/login', [loginController::class, 'login'])->name('login');
